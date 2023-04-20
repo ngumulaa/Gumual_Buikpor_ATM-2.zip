fc8bfa41c5bc56a3064d8f440e20b2f90898bb01
nicole gumula <ngumula@guilford.edu>
2023-04-20 18:04:36 -0400

    initial project

---
+++ project/src/main/java/edu/guilford/App.java
@@ -0,0 +1,19 @@
+package edu.guilford;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        // Create a new instance of the class
+        
+    }
+
+    
+    }
+}
